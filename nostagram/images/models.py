d14a956f6cac6e0670a278f15df81a59ad9ebef3
WOOJE <woojenoh@gmail.com>
2018-07-29 21:51:48 +0900

    create TimeStamp, Image, Comment, Like model

--- nostagram/images/models.py
+++ nostagram/images/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from nostagram.users import models as user_models
 
-# Create your models here.
+
+class TimeStampedModel(models.Model):
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Image(TimeStampedModel):
+
+    """ Image Model """
+
+    file = models.ImageField()
+    location = models.CharField(max_length=140)
+    caption = models.TextField()
+    creator = models.ForeignKey(user_models.User, on_delete=models.PROTECT, null=True)
+
+
+class Comment(TimeStampedModel):
+
+    """ Comment Model """
+
+    message = models.TextField()
+    creator = models.ForeignKey(user_models.User, on_delete=models.PROTECT, null=True)
+    image = models.ForeignKey(Image, on_delete=models.PROTECT, null=True)
+
+
+class Like(TimeStampedModel):
+
+    """ Like Model """
+
+    creator = models.ForeignKey(user_models.User, on_delete=models.PROTECT, null=True)
+    image = models.ForeignKey(Image, on_delete=models.PROTECT, null=True)
