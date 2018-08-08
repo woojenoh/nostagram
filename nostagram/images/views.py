e0c060176de17fb0c9a3417b098944d15c189bc2
WOOJE <woojenoh@gmail.com>
2018-08-08 20:52:38 +0900

    Getting the Feed

--- nostagram/images/views.py
+++ nostagram/images/views.py
@@ -3,22 +3,25 @@ from rest_framework.response import Response
 from . import models, serializers
 
 
-class ListAllImages(APIView):
+class Feed(APIView):
+
     def get(self, request, format=None):
-        all_images = models.Image.objects.all()
-        serializer = serializers.ImageSerializer(all_images, many=True)
-        return Response(data=serializer.data)
+        user = request.user
 
+        following_users = user.following.all()
 
-class ListAllComments(APIView):
-    def get(self, request, format=None):
-        all_comments = models.Comment.objects.all()
-        serializer = serializers.CommentSerializer(all_comments, many=True)
-        return Response(data=serializer.data)
+        image_list = []
 
+        for following_user in following_users:
 
-class ListAllLikes(APIView):
-    def get(self, request, format=None):
-        all_Likes = models.Like.objects.all()
-        serializer = serializers.LikeSerializer(all_Likes, many=True)
-        return Response(data=serializer.data)
+            user_images = following_user.images.all()[:2]
+
+            for image in user_images:
+
+                image_list.append(image)
+
+        sorted_list = sorted(image_list, key=lambda image: image.created_at, reverse=True)
+
+        serializer = serializers.ImageSerializer(sorted_list, many=True)
+
+        return Response(serializer.data)
