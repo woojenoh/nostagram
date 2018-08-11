a2f6effc7c2ef20077eed21cad54d4e003716ac6
WOOJE <woojenoh@gmail.com>
2018-08-11 17:51:54 +0900

    Creating a comment / Deleting a comment

--- nostagram/images/urls.py
+++ nostagram/images/urls.py
@@ -10,8 +10,18 @@ urlpatterns = [
         name='feed'
     ),
     url(
-        regex=r'(?P<image_id>[0-9]+)/like/',
+        regex=r'^(?P<image_id>[0-9]+)/likes/',
         view=views.LikeImage.as_view(),
         name='like_image'
+    ),
+    url(
+        regex=r'^(?P<image_id>[0-9]+)/comments/',
+        view=views.LikeImage.as_view(),
+        name='comment_image'
+    ),
+    url(
+        regex=r'comments/(?P<comment_id>[0-9]+)/$',
+        view=views.Comment.as_view(),
+        name='comment'
     )
 ]
