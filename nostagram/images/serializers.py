ee2df9898be5f55dbeb409844ead1ff4de7d798c
WOOJE <woojenoh@gmail.com>
2018-08-21 14:55:41 +0900

    Getting User Profile

--- nostagram/images/serializers.py
+++ nostagram/images/serializers.py
@@ -2,6 +2,16 @@ from rest_framework import serializers
 from . import models
 from nostagram.users import models as user_models
 
+class UserProfileImageSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = models.Image
+        fields = (
+            'id',
+            'file',
+            'comment_count',
+            'like_count'
+        )
 
 class FeedUserSerializer(serializers.ModelSerializer):
 
--- nostagram/users/admin.py
+++ nostagram/users/admin.py
@@ -12,7 +12,7 @@ class UserAdmin(auth_admin.UserAdmin):
 
     form = UserChangeForm
     add_form = UserCreationForm
-    fieldsets = (("User", {"fields": ("name", "followers", "following", 'profile_image')}),) + \
+    fieldsets = (("User", {"fields": ("name", "followers", "following", 'profile_image', 'bio', 'website')}),) + \
         auth_admin.UserAdmin.fieldsets
     list_display = ["username", "name", "is_superuser"]
     search_fields = ["name"]
