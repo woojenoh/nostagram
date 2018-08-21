843e468a5b7b1088eb66cb8f27cc46e26b231659
WOOJE <woojenoh@gmail.com>
2018-08-21 15:23:43 +0900

    Getting User Followers

--- nostagram/users/urls.py
+++ nostagram/users/urls.py
@@ -23,5 +23,10 @@ urlpatterns = [
         regex=r'^(?P<username>\w+)/$',
         view=views.UserProfile.as_view(),
         name='user_profile'
+    ),
+    url(
+        regex=r'^(?P<username>\w+)/followers/$',
+        view=views.UserFollowers.as_view(),
+        name='user_followers'
     )
 ]
