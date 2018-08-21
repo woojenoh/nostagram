bacda84d58a2e5f0b57e4a6f3a9328f61e68d349
WOOJE <woojenoh@gmail.com>
2018-08-21 20:34:42 +0900

    searching user by username

--- nostagram/users/urls.py
+++ nostagram/users/urls.py
@@ -20,18 +20,23 @@ urlpatterns = [
         name='unfollow_user'
     ),
     url(
-        regex=r'^(?P<username>\w+)/$',
-        view=views.UserProfile.as_view(),
-        name='user_profile'
-    ),
-    url(
         regex=r'^(?P<username>\w+)/followers/$',
         view=views.UserFollowers.as_view(),
         name='user_followers'
     ),
-        url(
+    url(
         regex=r'^(?P<username>\w+)/following/$',
         view=views.UserFollowing.as_view(),
         name='user_following'
+    ),
+    url(
+        regex=r'^search/$',
+        view=views.Search.as_view(),
+        name='search'
+    ),
+    url(
+        regex=r'^(?P<username>\w+)/$',
+        view=views.UserProfile.as_view(),
+        name='user_profile'
     )
 ]
