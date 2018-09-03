a884b19e379358885810a64d0811be06324e4796
WOOJE <woojenoh@gmail.com>
2018-09-04 00:04:35 +0900

    Moving the App Component and adding reset-css

--- frontend/src/ReactotronConfig.js
+++
@@ -1,8 +0,0 @@
-import Reactotron from "reactotron-react-js";
-import { reactotronRedux } from "reactotron-redux";
-
-Reactotron.configure({ name: "Nostagram" })
-  .use(reactotronRedux())
-  .connect();
-
-export default Reactotron;
--- frontend/src/redux/configureStore.js
+++ frontend/src/redux/configureStore.js
@@ -3,7 +3,6 @@ import thunk from "redux-thunk";
 import { routerReducer, routerMiddleware } from "react-router-redux";
 import createHistory from "history/createBrowserHistory";
 import users from "redux/modules/users";
-// import Reactotron from "ReactotronConfig";
 import { composeWithDevTools } from "redux-devtools-extension";
 import { i18nState } from "redux-i18n";
 
