4a293f63563bf0855d74c4819514c76f584b10f8
WOOJE <woojenoh@gmail.com>
2018-09-03 16:53:08 +0900

    Redux Middlewares: Thunk

--- frontend/src/redux/configureStore.js
+++ frontend/src/redux/configureStore.js
@@ -1,11 +1,15 @@
-import { createStore, combineReducers } from "redux";
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
 import users from "redux/modules/users";
+
+const middlewares = [thunk];
 
 // 리듀서를 합친다.
 const reducer = combineReducers({
   users
 });
 
-let store = initialState => createStore(reducer);
+let store = initialState =>
+  createStore(reducer, applyMiddleware(...middlewares));
 
 export default store();
