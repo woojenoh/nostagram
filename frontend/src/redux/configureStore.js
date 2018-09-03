24f3e947c4cb10c45c57debdf5665b3358041778
WOOJE <woojenoh@gmail.com>
2018-09-03 22:31:12 +0900

    Syncing React Router with Redux

--- frontend/src/redux/configureStore.js
+++ frontend/src/redux/configureStore.js
@@ -1,11 +1,15 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
+import { routerReducer, routerMiddleware } from "react-router-redux";
+import createHistory from "history/createBrowserHistory";
 import users from "redux/modules/users";
 
 // 현재 환경을 확인한다.
 const env = process.env.NODE_ENV;
 
-const middlewares = [thunk];
+const history = createHistory();
+
+const middlewares = [thunk, routerMiddleware(history)];
 
 // logger는 현재 환경이 개발일 때만 부른다.
 if (env === "development") {
@@ -15,10 +19,12 @@ if (env === "development") {
 
 // 리듀서를 합친다.
 const reducer = combineReducers({
-  users
+  users,
+  routing: routerReducer
 });
 
 let store = initialState =>
   createStore(reducer, applyMiddleware(...middlewares));
 
+export { history };
 export default store();
