186df4bd8adfed9352c1467511efd39cd3180f1d
WOOJE <woojenoh@gmail.com>
2018-09-04 21:29:54 +0900

    SignupForm

--- frontend/src/components/Auth/presenter.js
+++ frontend/src/components/Auth/presenter.js
@@ -14,14 +14,14 @@ const Auth = (props, context) => (
       </div>
       <div className={styles.whiteBox}>
         {props.action === "login" ? (
-          <p>
+          <p className={styles.text}>
             Don't have an account?{" "}
             <span className={styles.changeLink} onClick={props.changeAction}>
               Sign up
             </span>
           </p>
         ) : (
-          <p>
+          <p className={styles.text}>
             Have an account?{" "}
             <span className={styles.changeLink} onClick={props.changeAction}>
               Log in
