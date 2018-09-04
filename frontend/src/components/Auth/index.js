56546e5f730c467614d9ccdcdb39b44e1dc1b0de
WOOJE <woojenoh@gmail.com>
2018-09-04 19:50:58 +0900

    Auth Component part One

---
+++ frontend/src/components/Auth/index.js
@@ -0,0 +1,11 @@
+import { connect } from "react-redux";
+import Container from "./container";
+
+// Log in
+// Sign up
+// Recover password
+// Check username
+// Check password
+// Check email
+
+export default connect()(Container);
