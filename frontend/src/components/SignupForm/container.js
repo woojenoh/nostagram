4b1aa60e5a2acb9fe2ee9f175d8835d67c0eb5e7
WOOJE <woojenoh@gmail.com>
2018-09-05 22:31:12 +0900

    Connecting the forms to facebook login

--- frontend/src/components/SignupForm/container.js
+++ frontend/src/components/SignupForm/container.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 import SignupForm from "./presenter";
 
 class Container extends Component {
@@ -7,6 +8,10 @@ class Container extends Component {
     name: "",
     username: "",
     password: ""
+  };
+
+  static propTypes = {
+    facebookLogin: PropTypes.func.isRequired
   };
 
   render() {
@@ -39,8 +44,8 @@ class Container extends Component {
   };
 
   _handleFacebookLogin = response => {
-    console.log(response);
-    // redux action
+    const { facebookLogin } = this.props;
+    facebookLogin(response.accessToken);
   };
 }
 
