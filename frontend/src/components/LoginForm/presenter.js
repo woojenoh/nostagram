5d0889393bdd63b0f2b561bd6a6940221c458493
WOOJE <woojenoh@gmail.com>
2018-09-05 19:01:12 +0900

    Controlling inputs and submit on LoginForm

--- frontend/src/components/LoginForm/presenter.js
+++ frontend/src/components/LoginForm/presenter.js
@@ -5,16 +5,22 @@ import formStyles from "shared/formStyles.scss";
 
 const LoginForm = (props, context) => (
   <div className={formStyles.formComponent}>
-    <form className={formStyles.form}>
+    <form className={formStyles.form} onSubmit={props.handleSubmit}>
       <input
         type="text"
         placeholder={context.t("Username")}
         className={formStyles.textInput}
+        value={props.usernameValue}
+        onChange={props.handleInputChange}
+        name="username"
       />
       <input
         type="password"
         placeholder={context.t("Password")}
         className={formStyles.textInput}
+        value={props.passwordValue}
+        onChange={props.handleInputChange}
+        name="password"
       />
       <input
         type="submit"
@@ -35,6 +41,13 @@ const LoginForm = (props, context) => (
 
 LoginForm.contextTypes = {
   t: PropTypes.func.isRequired
+};
+
+LoginForm.propTypes = {
+  usernameValue: PropTypes.string.isRequired,
+  passwordValue: PropTypes.string.isRequired,
+  handleInputChange: PropTypes.func.isRequired,
+  handleSubmit: PropTypes.func.isRequired
 };
 
 export default LoginForm;
