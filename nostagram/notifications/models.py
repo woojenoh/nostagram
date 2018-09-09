cd6be1a79f5988aba5cf30c01658ca4148de4e3b
WOOJE <woojenoh@gmail.com>
2018-09-09 23:06:13 +0900

    Change on_delete(image in notifications models)

--- nostagram/notifications/models.py
+++ nostagram/notifications/models.py
@@ -2,22 +2,23 @@ from django.db import models
 from nostagram.users import models as user_models
 from nostagram.images import models as image_models
 
+
 class Notification(image_models.TimeStampedModel):
 
-  TYPE_CHOICES = (
-    ('like', 'Like'),
-    ('comment', 'Comment'),
-    ('follow', 'Follow')
-  )
+    TYPE_CHOICES = (
+        ('like', 'Like'),
+        ('comment', 'Comment'),
+        ('follow', 'Follow')
+    )
 
-  creator = models.ForeignKey(user_models.User, on_delete=models.PROTECT, related_name='creator')
-  to = models.ForeignKey(user_models.User, on_delete=models.PROTECT, related_name='to')
-  notification_type = models.CharField(max_length=20, choices=TYPE_CHOICES)
-  image = models.ForeignKey(image_models.Image, on_delete=models.PROTECT, null=True, blank=True)
-  comment = models.TextField(null=True, blank=True)
+    creator = models.ForeignKey(user_models.User, on_delete=models.PROTECT, related_name='creator')
+    to = models.ForeignKey(user_models.User, on_delete=models.PROTECT, related_name='to')
+    notification_type = models.CharField(max_length=20, choices=TYPE_CHOICES)
+    image = models.ForeignKey(image_models.Image, on_delete=models.CASCADE, null=True, blank=True)
+    comment = models.TextField(null=True, blank=True)
 
-  class Meta:
-    ordering = ['-created_at']
+    class Meta:
+        ordering = ['-created_at']
 
-  def __str__(self):
-    return 'From: {} - To: {}'.format(self.creator, self.to)
+    def __str__(self):
+        return 'From: {} - To: {}'.format(self.creator, self.to)
