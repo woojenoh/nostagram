ff258da52faae0f9b2299e30322b62b3720264b1
WOOJE <woojenoh@gmail.com>
2018-09-10 22:29:21 +0900

    Photo Components CSS

--- frontend/src/components/CommentBox/index.js
+++ frontend/src/components/CommentBox/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import PropTypes from "prop-types";
-import Textarea from "react-textarea-autosize";
 import styles from "./styles.scss";
 
 const CommentBox = (props, context) => (
-  <form>
-    <Textarea placeholder={context.t("Add a comment...")} />
+  <form className={styles.commentBox}>
+    <textarea
+      className={styles.input}
+      placeholder={context.t("Add a comment...")}
+    />
   </form>
 );
 
