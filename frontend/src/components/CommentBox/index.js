04f53bc46d45aba25f84b0376bbd5f40475c43f8
WOOJE <woojenoh@gmail.com>
2018-09-11 15:48:11 +0900

    CommentBox Textarea and font

--- frontend/src/components/CommentBox/index.js
+++ frontend/src/components/CommentBox/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styles from "./styles.scss";
+import Textarea from "react-textarea-autosize";
 
 const CommentBox = (props, context) => (
   <form className={styles.commentBox}>
-    <textarea
+    <Textarea
       className={styles.input}
       placeholder={context.t("Add a comment...")}
     />
